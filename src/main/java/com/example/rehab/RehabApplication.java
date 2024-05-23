c9322fc7e8843d4f335d003b3e4ac2fd99e75e2b
SuperTaeyun <super.kim.taeyun@gmail.com>
2024-05-23 14:06:19 +0900

    feat: Accessing Data with JPA 가이드 진행
    가이드 링크: https://spring.io/guides/gs/accessing-data-jpa

--- src/main/java/com/example/rehab/RehabApplication.java
+++ src/main/java/com/example/rehab/RehabApplication.java
@@ -1,5 +1,7 @@
 package com.example.rehab;
 
+import com.example.rehab.accessingdatajpa.Customer;
+import com.example.rehab.accessingdatajpa.CustomerRepository;
 import com.example.rehab.consumingrest.Quote;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -76,6 +78,41 @@ public class RehabApplication {
         return args -> {
             Quote quote = restTemplate.getForObject("http://localhost:8080/api/random", Quote.class);
             log.info(Objects.requireNonNull(quote).toString());
+        };
+    }
+
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return args -> {
+            // save a few customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            repository.findAll().forEach(customer -> {
+                log.info(customer.toString());
+            });
+            log.info("");
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findById(1L);
+            log.info("Customer found with findById(1L):");
+            log.info("-------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("-------------------------------");
+            repository.findByLastName("Bauer").forEach(bauer -> {
+                log.info(bauer.toString());
+            });
+            log.info("");
         };
     }
 
---
+++ src/main/java/com/example/rehab/accessingdatajpa/Customer.java
@@ -0,0 +1,43 @@
+package com.example.rehab.accessingdatajpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    protected Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Customer[id=%d, firstName='%s', lastName='%s']",
+                id, firstName, lastName);
+    }
+}
